e24de612098e306cade70958696d68cb68ba0c2d
ydobrov <yuradobr@i.ua>
2018-07-26 13:30:38 +0300

    Add key "Operation"

--- SysAppQuestionnaireRight Client Manager.sql
+++ SysAppQuestionnaireRight Client Manager.sql
@@ -3,6 +3,21 @@ BEGIN
   USING (SELECT "Id"
          FROM "ApplicationQuestionnaire") Source
   ON (Target."RecordId" = Source."Id" AND
+      Target."SysAdminUnitId" = '{7A53691B-926F-4B38-8C28-75D9DFC88F7C}' AND -- Клиентский менеджер
+      Target."Operation" = 1 --> EntitySchemaRecRightOperation --> Изменение
+      )
+WHEN NOT MATCHED THEN INSERT ("RecordId", "SysAdminUnitId", "Operation", "RightLevel", "Position", "SourceId")
+VALUES ( SOURCE."Id", '{7A53691B-926F-4B38-8C28-75D9DFC88F7C}', 1, 1, 0, '{8A248A03-E9A5-DF11-9989-485B39C18470}');
+-- "RightLevel" = 1 -> SysEntitySchemaRecOprRightLvl -> Разрешено
+-- "SourceId" = '{8A248A03-E9A5-DF11-9989-485B39C18470}' -> SysEntitySchemaRecRightSource -> Ручное управление
+END;
+ROLLBACK; -- COMMIT
+
+BEGIN
+  MERGE INTO "SysAppQuestionnaireRight" Target
+  USING (SELECT "Id"
+         FROM "ApplicationQuestionnaire") Source
+  ON (Target."RecordId" = Source."Id" AND
       Target."SysAdminUnitId" = '{7A53691B-926F-4B38-8C28-75D9DFC88F7C}') -- Клиентский менеджер
   WHEN MATCHED THEN UPDATE SET Target."Operation" = 1, --> EntitySchemaRecRightOperation --> Изменение
     Target."RightLevel" = 1, --> SysEntitySchemaRecOprRightLvl --> Разрешено
