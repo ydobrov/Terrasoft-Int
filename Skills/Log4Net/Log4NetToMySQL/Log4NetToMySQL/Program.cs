e7cfd5fccff21e3b820244b9b4004bfb6cd646e1
ydobrov <yuradobr@i.ua>
2018-10-09 10:05:46 +0300

    Log4Net naming

--- Skills/Log4Net/Log4NetToMySQL/Log4NetToMySQL/Program.cs
+++ Skills/Log4Net/Log4NetToMySQL/Log4NetToMySQL/Program.cs
@@ -4,19 +4,15 @@ using System.Linq;
 using System.Reflection;
 using log4net;
 
-
-
 namespace Log4NetToMySQL {
 	class Program {
 
-		//private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
 		static void Main(string[] args) {
-			//log4net.Config.XmlConfigurator.Configure();
-			ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-			log.Error("test", new Exception("test"));
+			ILog log = LogManager.GetLogger("Custom Logger");
+			Console.WriteLine(MethodBase.GetCurrentMethod().DeclaringType);
+			log.Error("test", new Exception("Test it"));
 			Console.WriteLine("Hit enter");
-			log.Error("Hello logging world!");
+			log.Error("Test it again");
 			Console.ReadLine();
 		}
 	}
